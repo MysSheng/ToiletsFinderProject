6e15c5860f3ee894d2eefa4a94cccb51ea75eb2e
MysSheng <liuzongzhe6705@gmail.com>
2024-05-24 20:58:56 +0800

    First Commit

---
+++ ToiletsFinderProject/src/main/java/com/JavaFinal/ToiletsFinder/ToiletsFinderProjectApplication.java
@@ -0,0 +1,13 @@
+package com.JavaFinal.ToiletsFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToiletsFinderProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToiletsFinderProjectApplication.class, args);
+	}
+
+}
---
+++ ToiletsFinderProject/src/test/java/com/JavaFinal/ToiletsFinder/ToiletsFinderProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JavaFinal.ToiletsFinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToiletsFinderProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
