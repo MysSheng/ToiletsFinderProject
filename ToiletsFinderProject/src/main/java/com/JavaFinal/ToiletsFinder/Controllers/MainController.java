5b45915767c12840c4b31a23ea704030dc587209
MysSheng <liuzongzhe6705@gmail.com>
2024-06-12 02:58:48 +0800

    implement updatepage and fix the distance's bud

--- ToiletsFinderProject/src/main/java/com/JavaFinal/ToiletsFinder/Controllers/MainController.java
+++ ToiletsFinderProject/src/main/java/com/JavaFinal/ToiletsFinder/Controllers/MainController.java
@@ -1,24 +1,39 @@
 package com.JavaFinal.ToiletsFinder.Controllers;
 
 import com.JavaFinal.ToiletsFinder.SQLOperations;
+import com.JavaFinal.ToiletsFinder.ToiletUpdateUpload;
 import com.JavaFinal.ToiletsFinder.Location;
+import com.JavaFinal.ToiletsFinder.locations;
 import com.JavaFinal.ToiletsFinder.models.getLocationModel;
 import com.JavaFinal.ToiletsFinder.models.tableCol;
+import com.JavaFinal.ToiletsFinder.models.updateInput;
 import com.JavaFinal.ToiletsFinder.models.userInput;
+import com.JavaFinal.ToiletsFinder.LocationRepository;
 import com.opencagedata.jopencage.JOpenCageGeocoder;
 import com.opencagedata.jopencage.model.JOpenCageForwardRequest;
 import com.opencagedata.jopencage.model.JOpenCageResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
 import com.JavaFinal.ToiletsFinder.DistanceOperation;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.Properties;
 
 @Controller
 public class MainController {
@@ -138,8 +153,78 @@ public class MainController {
         return "/keywordLocation";
     }
 
-    @GetMapping("/upload")
-    public String uploadPage(){
-        return "upload";
+    @GetMapping("/update")
+    public String showForm() {
+        return "update";
     }
+
+    @Autowired
+    private LocationRepository locationRepository;
+
+    @PostMapping("/submitLocation")
+    @ResponseBody
+    public ModelAndView submitLocation(
+            @RequestParam("longitude") String longitudeStr, 
+            @RequestParam("latitude") String latitudeStr,
+            @RequestParam("name") String name,
+            @RequestParam("free") String freeStr,
+            @RequestParam("comment") String comment) {
+                ModelAndView modelAndView = new ModelAndView();
+        try {
+            double longitude = Double.parseDouble(longitudeStr);
+            double latitude = Double.parseDouble(latitudeStr);
+            boolean free = Boolean.parseBoolean(freeStr);
+            locations location = new locations(longitude, latitude, name, comment, free,0,true,true,true);
+            System.out.println(location.getLongitude()+" "+location.getLatitude()+" "+location.getName());
+            //資料成功抓到，接下來是在資料庫建立一筆
+
+            //locationRepository.save(location);
+            String sql = "insert into locations (longitude, latitude, name,comment,isFree,floor,accessibility,isGenderFriendly,isDisabledFriendly) values(?, ?, ?, ?, ?, ?, ?, ? ,?)";
+            // 加载JDBC驱动
+            Properties properties = new Properties();
+            try (InputStream input = ToiletUpdateUpload.class.getClassLoader().getResourceAsStream("db.properties")) {
+                if (input == null) {
+                    System.out.println("Sorry, unable to find db.properties");                   
+                }
+                properties.load(input);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            String jdbcUrl = properties.getProperty("jdbc.url");
+            String user = properties.getProperty("jdbc.username");
+            String password = properties.getProperty("jdbc.password");
+            try{
+                Connection connection=null;
+                Statement statement = null;
+                connection = DriverManager.getConnection(jdbcUrl, user, password);
+                statement = connection.createStatement();
+                //使用數據庫
+                String useDatabaseSQL = "USE toilets";
+                statement.executeUpdate(useDatabaseSQL);
+
+                PreparedStatement stmt = connection.prepareStatement(sql);
+                stmt.setDouble(1, location.getLongitude());
+                stmt.setDouble(2, location.getLatitude());
+                stmt.setString(3, location.getName());
+                stmt.setString(4, location.getComment());
+                stmt.setBoolean(5, location.isFree());
+                stmt.setInt(6, location.getFloor());
+                stmt.setBoolean(7, location.isAccessibility());
+                stmt.setBoolean(8, location.isGenderFriendly());
+                stmt.setBoolean(9, location.isDisabledFriendly());
+                int result;
+                result = stmt.executeUpdate();
+            }catch(SQLException ex){
+                ex.printStackTrace();
+            }
+            modelAndView.setViewName("redirect:/mainpage");
+
+            return modelAndView;
+
+        } catch (NumberFormatException e) {
+            return modelAndView;
+        }
+    }
+
+
 }
---
+++ ToiletsFinderProject/src/main/java/com/JavaFinal/ToiletsFinder/LocationRepository.java
@@ -0,0 +1,6 @@
+package com.JavaFinal.ToiletsFinder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LocationRepository extends JpaRepository<locations, Long>{
+    
+}
